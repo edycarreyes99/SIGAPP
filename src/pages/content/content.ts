00a3089ae64fb52318211a7e0b393fe02b69eb24
Edycar Reyes <edycarreyes@gmail.com>
2018-12-07 12:09:37 -0600

    se configura la pagina para la lectura del sensor fingerprint

--- src/pages/content/content.ts
+++ src/pages/content/content.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, Platform,ModalController } from 'ionic-angular';
+import { FingerprintAIO, FingerprintOptions } from "@ionic-native/fingerprint-aio";
 
 @IonicPage()
 @Component({
@@ -8,6 +9,27 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class ContentPage {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+    public fingerprint: FingerprintAIO,
+    public modalCtrl: ModalController
+  ) { }
 
+
+  async fingerAuth() {
+    try {
+      await this.platform.ready();
+      const avaible = await this.fingerprint.isAvailable();
+      console.log(avaible);
+    } catch (e) {
+      const modal = this.modalCtrl.create({
+        title: "Error",
+        subtitle: "Fingerprint error",
+        message: e
+      });
+      modal.present();
+      console.log(e);
+    }
+  }
 }
