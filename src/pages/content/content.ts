e8fd66f7c44e1d827d97b34f753684055700d19d
Edycar Reyes <edycarreyes@gmail.com>
2018-12-07 13:18:56 -0600

    se configura correctamente el sensor de fingerprint

--- src/pages/content/content.ts
+++ src/pages/content/content.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, Platform,ModalController } from 'ionic-angular';
 import { FingerprintAIO, FingerprintOptions } from "@ionic-native/fingerprint-aio";
 
+
 @IonicPage()
 @Component({
   selector: 'page-content',
@@ -38,30 +39,4 @@ export class ContentPage {
       modal.present();
      });
    }
-  async fingerAuth() {
-    try {
-      await this.platform.ready();
-      const avaible = await this.fingerprint.isAvailable();
-      console.log(avaible);
-      if(avaible === "OK"){
-        const result = await this.fingerprint.show(this.FingerprintOptions);
-        const modal = this.modalCtrl.create({
-          title: "Sucsess",
-          subtitle: "Fingerprint Sucess",
-          message: result
-        });
-        modal.present();
-        this.navCtrl.push(MainPage);
-      }
-     
-    } catch (e) {
-      const modal = this.modalCtrl.create({
-        title: "Error",
-        subtitle: "Fingerprint error",
-        message: e
-      });
-      modal.present();
-      console.log(e);
-    }
-  }
 }
