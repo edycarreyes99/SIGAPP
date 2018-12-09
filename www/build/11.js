18ab57a06853ce7a43d5de47cfc46c62d5b06391
Edycar Reyes <edycarreyes@gmail.com>
2018-12-09 14:57:18 -0600

    se añade la carpeta www de compilaciones al repositorio

---
+++ www/build/11.js
@@ -0,0 +1,117 @@
+webpackJsonp([11],{
+
+/***/ 331:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ContentPageModule", function() { return ContentPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__ngx_translate_core__ = __webpack_require__(118);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(117);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__content__ = __webpack_require__(344);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+var ContentPageModule = /** @class */ (function () {
+    function ContentPageModule() {
+    }
+    ContentPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_3__content__["a" /* ContentPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["e" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_3__content__["a" /* ContentPage */]),
+                __WEBPACK_IMPORTED_MODULE_1__ngx_translate_core__["b" /* TranslateModule */].forChild()
+            ],
+            exports: [
+                __WEBPACK_IMPORTED_MODULE_3__content__["a" /* ContentPage */]
+            ]
+        })
+    ], ContentPageModule);
+    return ContentPageModule;
+}());
+
+//# sourceMappingURL=content.module.js.map
+
+/***/ }),
+
+/***/ 344:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ContentPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(117);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_fingerprint_aio__ = __webpack_require__(226);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_status_bar__ = __webpack_require__(120);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+var ContentPage = /** @class */ (function () {
+    function ContentPage(navCtrl, platform, fingerprint, modalCtrl, statusbar) {
+        this.navCtrl = navCtrl;
+        this.platform = platform;
+        this.fingerprint = fingerprint;
+        this.modalCtrl = modalCtrl;
+        this.statusbar = statusbar;
+        this.FingerprintOptions = {
+            clientId: 'SIGAPP',
+            clientSecret: 'password',
+            disableBackup: true
+        };
+        this.statusbar.hide();
+    }
+    ContentPage.prototype.login = function () {
+        var _this = this;
+        this.fingerprint.show({
+            clientId: 'Fingerprint-demo',
+            clientSecret: 'password'
+        }).then(function (result) {
+            _this.navCtrl.setRoot('LoginPage');
+        }).catch(function (err) {
+            var modal = _this.modalCtrl.create({
+                title: "err",
+                subtitle: "Fingerprint err",
+                message: err
+            });
+            modal.present();
+        });
+    };
+    ContentPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-content',template:/*ion-inline-start:"K:\Proyectos Web\SIGAPP\SIGAPP\src\pages\content\content.html"*/'<ion-header>\n\n  <ion-navbar color="primary">\n\n    <ion-title>\n\n      Fingerprint Authentication\n\n    </ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding>\n\n  <ion-row class="login-row" align-items-center>\n\n    <ion-col col-5></ion-col>\n\n    <ion-col col-2 class="lock-col">\n\n      <div class="lock-bg"></div>\n\n      <img src="../../assets/SIGAPP Logo/Logotipos/SIGAPP Logo Bandera Rounded.png" class="lock-img" tappable (click)="login()" alt="Logo SIGAPP">\n\n    </ion-col>\n\n    <ion-col col-5></ion-col>\n\n  </ion-row>\n\n</ion-content>'/*ion-inline-end:"K:\Proyectos Web\SIGAPP\SIGAPP\src\pages\content\content.html"*/
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* NavController */],
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* Platform */],
+            __WEBPACK_IMPORTED_MODULE_2__ionic_native_fingerprint_aio__["a" /* FingerprintAIO */],
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* ModalController */],
+            __WEBPACK_IMPORTED_MODULE_3__ionic_native_status_bar__["a" /* StatusBar */]])
+    ], ContentPage);
+    return ContentPage;
+}());
+
+//# sourceMappingURL=content.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=11.js.map
